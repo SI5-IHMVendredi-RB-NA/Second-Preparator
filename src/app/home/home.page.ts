086f82b8d315bb7b9ed0b947c5ee9a2e0254c448
AzzouziNouamane <nouamane.azzouzi@etu.unice.fr>
2021-03-02 22:14:13 +0100

    Affichage des boissons/desserts par quantite

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,6 +6,8 @@ import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import Speech from 'speak-tts';
 import {  ChangeDetectorRef } from '@angular/core';
+import { ELocalNotificationTriggerUnit, LocalNotifications } from '@ionic-native/local-notifications/ngx';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -19,7 +21,15 @@ export class HomePage {
   speech = new Speech();
   mic: any;
   micToggle =  false;
-  constructor(private http: HttpClient, private sseService: SseService, private cdr: ChangeDetectorRef) {
+  rushHour: boolean = false;
+  vagueDictionaryDessert = {};
+  vagueDictionaryDrink = {};
+  idVague = 0;
+  textPerVague = {};
+  MAX_VAGUES_ORDERS = 5;
+  
+  constructor(private http: HttpClient, private sseService: SseService, private cdr: ChangeDetectorRef,
+    private localNotification: LocalNotifications, private plt: Platform) {
     // let source = new EventSource('http://localhost:9428/api/repas/sse');
     // source.addEventListener('message', aString => console.log(aString.data), false);
    /*this.tmp = this.http.get<Repas[]>('http://localhost:9428/api/repas');
@@ -34,6 +44,19 @@ export class HomePage {
   }
 
   ngOnInit(){
+
+    this.plt.ready().then(() => {
+        this.localNotification.on('click').subscribe(res => {
+          let msg = res.data? res.data.mydata : '';
+          console.log(msg);
+        });
+  
+        this.localNotification.on('trigger').subscribe(res => {
+          let msg = res.data? res.data.mydata : '';
+          console.log(msg);
+        });
+      });
+
       if (this.speech.hasBrowserSupport()) { // returns a boolean
           console.log('speech synthesis supported');
       }
@@ -46,18 +69,42 @@ export class HomePage {
       }).catch(e => {
           console.error('An error occured while initializing : ', e);
       });
-      this.http.get<any[]>('http://localhost:9428/api/order').subscribe( v => {
+/*       this.http.get<any[]>('http://localhost:9428/api/order').subscribe( v => {
       this.orders = v ;
-    });
+    }); */
       this.sseService
-        .getServerSentEvent('http://localhost:9428/api/order/stream')
+        .getServerSentEvent('http://10.189.174.180:9428/api/order/stream')
         .subscribe(data => {
           console.log(data);
           const order = JSON.parse(data.data);
           this.orders.push(order.order);
-          this.addOrderToVague(order.order);
+          this.orderToVague(order.order);
+          this.isRushHour();
+          this.addOrderToDictionaryDessert(order.order);
+          this.addOrderToDictionaryDrink(order.order);
           console.log(this.vagues);
         });
+
+        this.sseService
+        .getServerSentEvent('http://10.189.174.180:9428/api/preparator/stream')
+        .subscribe(data => {
+          console.log(JSON.parse(data.data));
+          console.log('Vague terminée par ' + JSON.parse(data.data).name_preparator);
+          this.scheduleNotification('Vague terminée par ' + JSON.parse(data.data).name_preparator);
+          //const order = JSON.parse(data.data);
+          //this.orders.push(order.order);
+          //this.scheduleNotification('Votre commande est prête à être retirée !');
+        });
+  }
+
+  scheduleNotification(message: string) {
+    this.localNotification.schedule({
+      id: 1,
+      title: 'Etat de votre commande',
+      text: message,
+      data: {page: 'myPage'},
+      trigger: {in: 5, unit: ELocalNotificationTriggerUnit.SECOND}
+    })
   }
 
   async textToSpeech(){
@@ -175,7 +222,7 @@ export class HomePage {
       this.orders.splice(index, 1);
       this.cdr.detectChanges();
 
-      this.http.post<any[]>('http://localhost:9428/api/user', order).subscribe( v => {
+      this.http.post<any[]>('http://10.189.174.180:9428/api/user', order).subscribe( v => {
           console.log(v);
         //this.orders = v ;
       });
@@ -186,45 +233,109 @@ export class HomePage {
     this.vagues.splice(index, 1);
     this.cdr.detectChanges();
 
-    this.http.post<any[]>('http://localhost:9428/api/user/vague', vague.order1).subscribe( v => {
-      console.log(v);
-    });
+    vague.orders.forEach(order => {
+      this.http.post<any[]>('http://localhost:9428/api/user/vague', order).subscribe( v => {
+        console.log(v);
+      });
+    })
 
-    this.http.post<any[]>('http://localhost:9428/api/user/vague', vague.order2).subscribe( v => {
-      console.log(v);
+    this.http.post<any[]>('http://localhost:9428/api/preparator/vague', {id: 2, name: 'Remy'}).subscribe( v => {
+        console.log(v);
+      });
+  }
+
+  isRushHour(): void {
+    if(this.orders.length > this.MAX_VAGUES_ORDERS) {
+      this.rushHour = true;
+    } 
+    else{
+      this.rushHour = false;
+    }
+}
+
+getDessertsPerVague(vague): string {
+  let result = "";
+  console.log(this.vagueDictionaryDessert);
+  for(var key in this.vagueDictionaryDessert[vague.id]) {
+    const value = this.vagueDictionaryDessert[vague.id][key];
+    result += value + "x " + key + "\n";
+  }
+  this.textPerVague[vague.id] = result;
+  return result;
+}
+
+getDrinksPerVague(vague): string {
+  let result = "";
+  console.log(this.vagueDictionaryDrink);
+  for(var key in this.vagueDictionaryDrink[vague.id]) {
+    const value = this.vagueDictionaryDrink[vague.id][key];
+    result += value + "x " + key + "\n";
+  }
+  this.textPerVague[vague.id] = result;
+  return result;
+}
+
+  addOrderToDictionaryDessert(newOrder) {
+    const currentVague = this.vagues[this.vagues.length - 1];
+    console.log(currentVague);
+    currentVague.orders.forEach(order => {
+      console.log(order === newOrder);
+      if(order === newOrder) {
+        if(this.vagueDictionaryDessert[currentVague.id] === undefined) {
+          this.vagueDictionaryDessert[currentVague.id] = {};
+        }
+        if(this.vagueDictionaryDessert[currentVague.id][order.repas.dessert + ""] === undefined) {
+          this.vagueDictionaryDessert[currentVague.id][order.repas.dessert + ""] = 1;
+        }
+        else {
+          this.vagueDictionaryDessert[currentVague.id][order.repas.dessert + ""] += 1;
+        }
+      }
     });
   }
 
-  isRushHour(): boolean {
-      if(this.orders.length > 2) return true;
-      return false;
+  addOrderToDictionaryDrink(newOrder) {
+    const currentVague = this.vagues[this.vagues.length - 1];
+    console.log(currentVague);
+    currentVague.orders.forEach(order => {
+      console.log(order === newOrder);
+      if(order === newOrder) {
+        if(this.vagueDictionaryDrink[currentVague.id] === undefined) {
+          this.vagueDictionaryDrink[currentVague.id] = {};
+        }
+        if(this.vagueDictionaryDrink[currentVague.id][order.repas.boisson + ""] === undefined) {
+          this.vagueDictionaryDrink[currentVague.id][order.repas.boisson + ""] = 1;
+        }
+        else {
+          this.vagueDictionaryDrink[currentVague.id][order.repas.boisson + ""] += 1;
+        }
+      }
+    });
   }
 
-  addOrderToVague(order) {
-      console.log(this.vagues.length);
-      if(this.vagues.length === 0) {
-          let vague: Vague = new Vague;
-          vague.order1 = null;
-          vague.order2 = null;
-          vague.order1 = order;
-          this.vagues.push(vague);
-      }
-      else {
-          if(this.vagues[this.vagues.length - 1].order1 === null) {
-            this.vagues[this.vagues.length - 1].order1 = order;
-          }
-          else if(this.vagues[this.vagues.length - 1].order2 === null) {
-            this.vagues[this.vagues.length - 1].order2 = order;
-          }
-
-          else {
-            let vague: Vague = new Vague;
-            vague.order1 = null;
-            vague.order2 = null;
-            vague.order1 = order;
-            this.vagues.push(vague);
-          }
-      }
+  orderToVague(order) {
+    console.log(order);
+    if(this.vagues.length === 0) {
+      let vague: Vague = new Vague;
+      vague.id = this.idVague;
+      vague.orders = [];
+      vague.orders.push(order);
+      this.vagues.push(vague);
+      console.log(this.vagues);
+    }
+    else if(this.vagues[this.vagues.length - 1].orders.length === this.MAX_VAGUES_ORDERS) {
+      this.idVague+= 1;
+      let vague: Vague = new Vague;
+      vague.id = this.idVague;
+      vague.orders = [];
+      vague.orders.push(order);
+      this.vagues.push(vague);
+    }
+    else {
+      this.vagues[this.vagues.length - 1].orders.push(order);
+    }
+    console.log(this.vagues.length);
+    console.log(this.vagues[this.vagues.length - 1]);
   }
 
 }
